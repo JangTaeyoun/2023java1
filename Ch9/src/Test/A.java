75b3025feef5243091c3ec796a21613fac55de7f
JangTaeYoung <147689082+JangTaeyoun@users.noreply.github.com>
2023-11-08 21:10:06 +0900

    Update A.java

--- Ch9/src/Test/A.java
+++ Ch9/src/Test/A.java
@@ -1,21 +1,48 @@
 package Test;
 
 public class A {
-	static int memberA = 10;
+	int field1 = 10;
+	static int field2 = 20;
+	B b = new B();
+	C c = new C();
+	//static B b1 = new B();
+	static C c1 = new C();
+	
+	void method1() {
+		B b = new B();
+		C c = new C();
+	}
+	static void method2() {
+		//B b = new B();
+		C c = new C();
+	}
+
+static int memberA = 10;
 	
 	A(){
 		System.out.println("A instance is generated.");
 	}
 	
-	static class B {
+	class B {
+		void method() {
+			field1 = 30;
+			method1();
+			field2 = 40;
+			method2();
+		}
+	}
+			
 		B(){
 			System.out.println("B instance is generated.");
 		}
 	}
 	
 	static class C {
-		C(){
-			System.out.println("C instance is generated.");
+		void method() {
+			//field1 = 30;
+			//method1();
+			field2 = 40;
+			method2();
 		}
 	}
 	void method1() {
